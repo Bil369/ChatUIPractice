6369dea7b0e06a8f252ab84f0901ad8ab62d5204
Song Zhuoxue <38159527+Bil369@users.noreply.github.com>
2021-02-17 22:02:56 +0800

    Basic ui

--- app/src/main/java/com/example/chatuipractice/MainActivity.kt
+++ app/src/main/java/com/example/chatuipractice/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.chatuipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val msgList = ArrayList<Msg>()
+
+    private var adapter: MsgAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMsg()
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        adapter = MsgAdapter(msgList)
+        recyclerView.adapter = adapter
+        sendButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v)
+        {
+            sendButton -> {
+                val content = inputText.text.toString()
+                if(content.isNotEmpty())
+                {
+                    val msg = Msg(content, Msg.TYPE_SENT)
+                    msgList.add(msg)
+                    adapter?.notifyItemInserted(msgList.size - 1)
+                    recyclerView.scrollToPosition(msgList.size - 1)
+                    inputText.setText("")
+                }
+            }
+        }
+    }
+
+    private fun initMsg()
+    {
+        val msg1 = Msg("今天又是啥都没干的一天", Msg.TYPE_RECEIVED)
+        msgList.add(msg1)
     }
 }
---
+++ app/src/main/java/com/example/chatuipractice/Msg.kt
@@ -0,0 +1,9 @@
+package com.example.chatuipractice
+
+class Msg(val content: String, val type: Int) {
+    companion object
+    {
+        const val TYPE_SENT = 0
+        const val TYPE_RECEIVED = 1
+    }
+}
